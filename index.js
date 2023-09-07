cc29cd44e0fa8871f1f6daf7f725c14a9027c7b1
CodeLordGh <babohkossi228@icloud.com>
2023-09-07 13:15:45 +0000

    Dark mode removed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Select The Elements
-let toggle_btn;
 let big_wrapper;
 let hamburger_menu;
 
@@ -18,7 +17,6 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el))
 
 function declare() {
-  toggle_btn = document.querySelector(".toggle-btn");
   big_wrapper = document.querySelector(".big-wrapper");
   hamburger_menu = document.querySelector(".hamburger-menu");
 }
@@ -26,37 +24,7 @@ function declare() {
 const main = document.querySelector("main");
 
 declare();
-
-let dark = false;
-
-function toggleAnimation() {
-  // Clone the wrapper
-  dark = !dark;
-  let clone = big_wrapper.cloneNode(true);
-  if (dark) {
-    clone.classList.remove("light");
-    clone.classList.add("dark");
-  } else {
-    clone.classList.remove("dark");
-    clone.classList.add("light");
-  }
-  clone.classList.add("copy");
-  main.appendChild(clone);
-
-  document.body.classList.add("stop-scrolling");
-
-  clone.addEventListener("animationend", () => {
-    document.body.classList.remove("stop-scrolling");
-    big_wrapper.remove();
-    clone.classList.remove("copy");
-    // Reset Variables
-    declare();
-    events();
-  });
-}
-
 function events() {
-  toggle_btn.addEventListener("click", toggleAnimation);
   hamburger_menu.addEventListener("click", () => {
     big_wrapper.classList.toggle("active");
   });
